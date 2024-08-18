df9dee9d1c7b5834ff65245f07604fc28a3e969e
Afo <obaone01@gmail.com>
2024-08-17 18:44:42 -0700

    cleared constant not yet set with 0x0 address or bytes added common error messages added enum for capitals

--- packages/contracts/src/Constants.sol
+++ packages/contracts/src/Constants.sol
@@ -2,16 +2,31 @@
 pragma solidity ^0.8.25;
 
 // EAS (ETHEREUM ATTESTATION SERVICE)
-address constant EAS_ARB = 0x4200000000000000000000000000000000000021; // Any OP Stack deployed
+address constant EAS_ARB = 0xbD75f629A22Dc1ceD33dDA0b68c546A1c035c458;
+address constant EAS_SEPOLIA = 0xC2679fBD37d54388Ce493F1DB75320D236e1815e;
 
-bytes32 constant actionSchemaUid = 0xfcd67741f543211da27d50ef5b1f3d0a648a9760cc84414b037c1c011ed883a6;
-bytes32 constant workSchemaUid = 0xb6e2f5905e3cce83d9713559ae8931940ac487fd4467a5134d8501eb2e339e81;
-bytes32 constant workApprovalSchemaUid = 0xb6e2f5905e3cce83d9713559ae8931940ac487fd4467a5134d8501eb2e339e81;
+bytes32 constant workSchemaUid = 0x0;
+bytes32 constant workApprovalSchemaUid = 0x0;
 
 address constant ActionRegistry = 0xa547526412e87fBAD5B483bd17F6540a1dC686fd;
-address constant WorkResolver = 0xd76a4D50F1CcaD941B85692Dc6681b35bC6B480c;
-address constant WorkApprovalResolver = 0xd76a4D50F1CcaD941B85692Dc6681b35bC6B480c;
+address constant WorkResolver = 0x0000000000000000000000000000000000000000;
+address constant WorkApprovalResolver = 0x0000000000000000000000000000000000000000;
 
+// ERROR MESSAGES
+error NotGardenAccount();
+error NotGardenerAccount();
+error NotInActionRegistry();
 
+// ENUMS
+enum Capital {
+  SOCIAL,
+  MATERIAL,
+  FINANCIAL,
+  LIVING,
+  INTELLECTUAL,
+  EXPERIENTIAL,
+  SPIRITUAL,
+  CULTURAL
+}
 
 
