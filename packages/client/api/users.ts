e89f68137bd13a07fa3e9f15f6872a826c69e270
Afo <obaone01@gmail.com>
2024-08-29 18:05:32 -0700

    fixed environment variables for get users function

--- packages/client/api/users.ts
+++ packages/client/api/users.ts
@@ -4,10 +4,10 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      VITE_PRIVY_APP_ID: string;
-      PINATA_API_KEY: string;
-      PINATA_API_SECRET: string;
-      PRIVY_APP_SECRET_IDL: string;
+      VITE_PRIVY_APP_ID?: string;
+      PINATA_API_KEY?: string;
+      PINATA_API_SECRET?: string;
+      PRIVY_APP_SECRET_ID?: string;
     }
   }
 }
@@ -17,8 +17,8 @@ export const config = {
 };
 
 const privy = new PrivyClient(
-  process.env.VITE_PRIVY_APP_ID,
-  process.env.PINATA_API_SECRET
+  process.env.VITE_PRIVY_APP_ID!,
+  process.env.PRIVY_APP_SECRET_ID!
 );
 
 export default async function handler(
