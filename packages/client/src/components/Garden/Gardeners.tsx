439174c38a6e7695e12d8a4a5353eff8998763f7
Afo <contact@afolabi.info>
2025-02-01 21:58:06 -0800

    updated gardener list views

--- packages/client/src/components/Garden/Gardeners.tsx
+++ packages/client/src/components/Garden/Gardeners.tsx
@@ -1,7 +1,7 @@
-import { Gardener } from "@/providers/garden";
+import { formatAddress } from "@/utils/text";
 
 interface GardenGardenersProps {
-  gardeners: Gardener[];
+  gardeners: GardenerCard[];
 }
 
 export const GardenGardeners: React.FC<GardenGardenersProps> = ({
@@ -11,14 +11,26 @@ export const GardenGardeners: React.FC<GardenGardenersProps> = ({
     <ul className="flex flex-col gap-2">
       {gardeners.length ?
         gardeners.map((user) => (
-          <li key={user.id}>
+          <li
+            key={user.id}
+            className="flex items-center gap-3 border-slate-100 border-2 shadow-sm rounded-lg p-2"
+          >
             <img
-              src={user.farcaster?.pfp ?? "/images/avatar.png"}
+              className="w-10 h-10"
+              src={user.avatar ?? "/images/avatar.png"}
               alt="Profile"
             />
-            <span>
-              {user?.email?.address || user.phone?.number || "Unknown User"}
-            </span>
+            <div className="flex flex-col">
+              <span className="font-semibold">
+                {(user.account && formatAddress(user.account)) ||
+                  user?.email ||
+                  user?.phone ||
+                  "Unknown User"}
+              </span>
+              <span className="text-xs">
+                Registered: {user.registeredAt.toDateString()}
+              </span>
+            </div>
           </li>
         ))
       : <p className="grid place-items-center p-8 text-center text-sm italic">
