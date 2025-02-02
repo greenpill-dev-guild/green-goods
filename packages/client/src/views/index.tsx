98df434e01b4d3237817aff5a760a4929ac8827b
Afo <contact@afolabi.info>
2025-02-01 21:54:32 -0800

    added new sub routes to view

--- packages/client/src/views/index.tsx
+++ packages/client/src/views/index.tsx
@@ -4,15 +4,23 @@ import Work from "./Work";
 import Profile from "./Profile";
 import Gardens from "./Gardens";
 import { Garden } from "./Gardens/Garden";
+import { GardenAssessment } from "./Gardens/Assessment";
+import { GardenWorkApproval } from "./Gardens/WorkApproval";
 
 export default function Views() {
   return (
     <main
-      className={`overflow-hidden h-[calc(100dvh-3.5rem)] max-h-[calc(100dvh-3.5rem)]`}
+      className={`overflow-hidden overscroll-contain h-[calc(100lvh-3.5rem)]`}
     >
       <Routes>
         <Route path="gardens" element={<Gardens />}>
-          <Route path=":id" element={<Garden />} />
+          <Route path=":id" element={<Garden />}>
+            <Route path="work/:workId" element={<GardenWorkApproval />} />
+            <Route
+              path="assessments/:assessmentId"
+              element={<GardenAssessment />}
+            />
+          </Route>
         </Route>
         <Route path="garden" element={<Work />} />
         <Route path="profile" element={<Profile />} />
