f397cc0236b68efba082ec6a0f45dc7ddb49d45f
Afo <contact@afolabi.info>
2025-03-28 23:30:33 -0700

    update

--- packages/client/dev-dist/sw.js
+++ packages/client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c982e567'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aa37vi39b3o"
+    "revision": "0.lhd6o8me7i"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
