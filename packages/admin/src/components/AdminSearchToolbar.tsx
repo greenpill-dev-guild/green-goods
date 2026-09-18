fe3414a68eb22edadff6f0ca21baf148f14afa9f
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2026-09-18 16:45:16 -0300

    ui(admin) remove focus shadow on manage member search input

--- packages/admin/src/components/AdminSearchToolbar.tsx
+++ packages/admin/src/components/AdminSearchToolbar.tsx
@@ -93,7 +93,7 @@ export function AdminSearchToolbar({
           className={cn(
             "h-full min-w-0 flex-1 bg-transparent px-3",
             "text-body-md text-[rgb(var(--m3-on-surface))]",
-            "outline-none border-none focus:outline-none"
+            "outline-none border-none focus:outline-none focus:shadow-none"
           )}
         />
 
